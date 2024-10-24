0bd20500d3d663a4adb522a3503de5d5b6bb9b66
Valeriia Novikova <lerumiu@gmail.com>
2024-10-25 00:19:13 +0200

    test

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, CardFooter, CardHeader, Divider, Heading, Text } from '@chakra-ui/react';
-import moment from 'moment/moment';
+import moment from 'moment/moment'; // dghdhgfhgs
 
 export default function Note({title, description, createdAt}){
     return (
